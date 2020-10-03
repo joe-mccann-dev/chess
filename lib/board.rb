b9dd0082424fddfb1b88ca4a4831330fe22df47c
Tenacious-Qi <joe.mccann@mailbox.org>
2020-10-02 23:57:46 -0400

    starts framing how to deal with pieces of same type that both can go to destination

--- lib/board.rb
+++ lib/board.rb
@@ -20,12 +20,14 @@ class Board
   def make_initial_board
     @squares = [
       black_row,
-      Array.new(8) { |c| Pawn.new(2, [1, c]) },
+      # Array.new(8) { |c| Pawn.new(2, [1, c]) },
       Array.new(8) { ' ' },
       Array.new(8) { ' ' },
       Array.new(8) { ' ' },
       Array.new(8) { ' ' },
-      Array.new(8) { |c| Pawn.new(1, [6, c]) },
+      Array.new(8) { ' ' },
+      Array.new(8) { ' ' },
+      # Array.new(8) { |c| Pawn.new(1, [6, c]) },
       white_row
     ]
   end
@@ -70,26 +72,41 @@ class Board
 
   def find_piece(player_color, piece_type)
     if player_color == :white
-      find_white_piece(player_color, piece_type)
+      find_white_piece(piece_type)
     else
-      find_black_piece(player_color, piece_type)
+      find_black_piece(piece_type)
     end
   end
 
-  def find_white_piece(player_color, piece_type)
-    white_pieces_that_go_to_dest.each do |piece|
-      assign_start_location(piece)
-      return piece if piece.instance_of?(piece_type) &&
-        valid_move?(@start_row, @start_column, player_color, piece)
+  def find_white_piece(piece_type)
+    pieces = white_pieces_that_go_to_dest.select do |piece|
+      # assign_start_location(piece)
+      piece.instance_of?(piece_type) &&
+        valid_move?(piece.location[0], piece.location[1], :white, piece)
     end
-    nil
+    puts "start_row: #{@start_row}"
+    puts "start_column: #{@start_column}"
+    p pieces
+    if pieces.length > 1
+      assign_start_location(pieces[1])
+      pieces[1]
+    else
+      # ask player which piece they want to move, a or b. 
+      # going to need the location, maybe use the column?
+      # puts 'two pieces can move to that location'
+      # puts' 
+      # selection = gets.chomp
+      # if selection matches column, move the piece in that column.
+      assign_start_location(pieces[0])
+      pieces[0]
+    end
   end
 
-  def find_black_piece(player_color, piece_type)
+  def find_black_piece(piece_type)
     black_pieces_that_go_to_dest.each do |piece|
       assign_start_location(piece)
       return piece if piece.instance_of?(piece_type) &&
-        valid_move?(@start_row, @start_column, player_color, piece)
+        valid_move?(@start_row, @start_column, :black, piece)
     end
     nil
   end
