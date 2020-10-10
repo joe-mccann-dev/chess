fd50b13710223bf28c70af1185ed8dc14feffe33
Tenacious-Qi <joe.mccann@mailbox.org>
2020-10-10 19:09:10 -0400

    removes upcase and downcase methods from InputValidator d/t bug with bishop attacking being interpreted as a pawn attack

--- lib/modules/input_validator.rb
+++ lib/modules/input_validator.rb
@@ -14,27 +14,27 @@ module InputValidator
   end
 
   def valid_attack_move?(move)
-    move[0].upcase.match?(/R|N|B|Q|K/) &&
-      move[1].downcase.match?('x') &&
-      move[2].downcase.match?(/[a-h]/) &&
+    move[0].match?(/R|N|B|Q|K/) &&
+      move[1].match?('x') &&
+      move[2].match?(/[a-h]/) &&
       move[3].match?(/[1-8]/)
   end
 
   def valid_pawn_attack?(move)
-    move[0].downcase.match?(/[a-h]/) &&
-    move[1].downcase.match?('x') &&
-    move[2].downcase.match?(/[a-h]/) &&
+    move[0].match?(/[a-h]/) &&
+    move[1].match?('x') &&
+    move[2].match?(/[a-h]/) &&
     move[3].match?(/[1-8]/)
   end
 
   def valid_pawn_move?(move)
-    move[0].downcase.match?(/[a-h]/) &&
+    move[0].match?(/[a-h]/) &&
       move[1].match?(/[1-8]/)
   end
 
   def valid_character_move?(move)
-    move[0].upcase.match?(/R|N|B|Q|K/) &&
-      move[1].downcase.match?(/[a-h]/) &&
+    move[0].match?(/R|N|B|Q|K/) &&
+      move[1].match?(/[a-h]/) &&
       move[2].match?(/[1-8]/)
   end
 end
