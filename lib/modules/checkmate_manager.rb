997f8242119d05df81d81e387ce72c5d124b21ff
Tenacious-Qi <joe.mccann@mailbox.org>
2020-10-22 19:55:35 -0400

    checks if players own move puts them in check

--- lib/modules/checkmate_manager.rb
+++ lib/modules/checkmate_manager.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module CheckmateManager
-
   def check?
     white_king.in_check || black_king.in_check
   end
@@ -16,18 +15,24 @@ module CheckmateManager
 
   def check_if_move_results_in_check(player_color)
     reassign_relevant_board_variables(player_color)
+    mark_king_as_in_check?(player_color)
+  end
+
+  def mark_king_as_in_check?(player_color)
     if player_color == :white
-      @black_king_in_check = white_pieces.any? do |piece|
-        attack_rules_followed?(piece.location[0], piece.location[1], player_color, piece)
-      end
+      @black_king_in_check = white_puts_black_in_check?(player_color)
       puts 'black in check' if @black_king_in_check
     else
-      @white_king_in_check = black_pieces.any? do |piece|
-        attack_rules_followed?(piece.location[0], piece.location[1], player_color, piece)
-      end
+      @white_king_in_check = black_puts_white_in_check?(player_color)
       puts 'white in check' if @white_king_in_check
     end
     @target.mark_as_in_check if @black_king_in_check || @white_king_in_check
+  end
+
+  def check_if_players_own_move_puts_them_in_check(player_color)
+    opposite_color = player_color == :white ? :black : :white
+    reassign_relevant_board_variables(opposite_color)
+    check_if_move_results_in_check(opposite_color)
   end
 
   def reassign_relevant_board_variables(player_color)
@@ -43,12 +48,15 @@ module CheckmateManager
     @attack_move = true
   end
 
-  # def move_puts_player_in_check?(piece, player_color)
-  #   if player_color == :black
-  #     piece.update_location(@dest_row, @dest_column)
-  #     result = white_pieces.any? { |piece| attack_rules_followed?(piece.location[0], piece.location[1], :black, black_king) }
-  #   end
-  #   piece.update_location(@start_row, @start_column)
-  #   result
-  # end
+  def white_puts_black_in_check?(player_color)
+    white_pieces.any? do |piece|
+      attack_rules_followed?(piece.location[0], piece.location[1], player_color, piece)
+    end
+  end
+
+  def black_puts_white_in_check?(player_color)
+    black_pieces.any? do |piece|
+      attack_rules_followed?(piece.location[0], piece.location[1], player_color, piece)
+    end
+  end
 end 
