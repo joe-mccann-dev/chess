d7b5e19d616089dcf212ef5096fdf78a0c9c9ac3
Tenacious-Qi <joe.mccann@mailbox.org>
2020-12-04 19:25:56 -0500

    adds a help method

--- lib/modules/display.rb
+++ lib/modules/display.rb
@@ -5,12 +5,52 @@ module Display
   WHITE = "\u265F".colorize(:light_yellow).freeze
   BLACK = "\u265F".colorize(:cyan).freeze
 
-  def show_welcome_message
+  def show_help
     puts <<-HEREDOC
 
- Welcome to Chess!
+    This game uses traditional algebraic notation to enter moves.
+    Moves are case sensitive.
 
- Win by placing your opponent's King in checkmate.
+    Pawns:
+
+      Regular pawn move: "e5"
+        To move a pawn, enter its file (column) in lower case, followed by the rank (row) to which you want to move it.
+        To move from e2 to e4, for example, then you would simply enter 'e4'
+    
+      Pawn attack move: "exd5"
+        To attack with a pawn, enter its file followed by an 'x' and the rank and file of the piece that you wish to attack.
+        To attack the piece at d5, enter 'exd5'
+    
+    All other pieces: "Qh5", "Qxh5", "Ke7", "Nxc3", etc.
+
+      Each piece is assigned a piece prefix:
+      
+        King   = K
+        Queen  = Q
+        Rook   = R
+        Knight = N
+        Bishop = B
+      
+      Regular move:
+        In uppercase, type the piece prefix of the piece you'd like to move, followed by the desired file and rank.
+        To move your Queen to h5, for example, then you would enter 'Qh5' and so on for the other pieces.
+
+      Attack move:
+        Similar to above, but preface your destination file and rank with a lowercase 'x'.
+        For example, if there is an opponent piece at h5 and you wish to attack it with your Queen,
+        enter 'Qxh5'
+
+    Special moves:
+
+      Castling: 
+        To King-side castle, enter '0-0'
+        To Queen-side castle, enter '0-0-0'
+
+      En Passant:
+        Perform the attack as if you are attacking the empty square behind the pawn eligible for an en passant attack
+        For example, your pawn is at e5, opponent pawn is at d5.
+        enter 'exd6' to capture the opponent pawn at d5
+        
 
     HEREDOC
   end
