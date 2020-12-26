702c5735143030e35a91b83693c86177f3ab3c4f
Tenacious-Qi <joe.mccann@mailbox.org>
2020-12-26 14:23:45 -0500

    simplify row_moves and col_moves for bishop, queen, rook

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -2,21 +2,34 @@
 
 # contains code for legal Queen movements
 class Queen < Piece
-
   def initialize(color, location, unicode = '♛')
     super
     @prefix = 'Q'
   end
 
   def row_moves
-    # rook row_moves + bishop row_moves
-    (1..7).to_a + (-7..-1).to_a + Array.new(14) { 0} +
-      (1..7).to_a + (-7..-1).to_a.reverse + (1..7).to_a + (-7..-1).to_a.reverse
+    [
+      1,  2,  3,  4,  5,  6,  7,
+     -1, -2, -3, -4, -5, -6, -7,
+      1,  2,  3,  4,  5,  6,  7,
+     -1, -2, -3, -4, -5, -6, -7,
+      1,  2,  3,  4,  5,  6,  7,
+     -7, -6, -5, -4, -3, -2, -1,
+      0,  0,  0,  0,  0,  0,  0,
+      0,  0,  0,  0,  0,  0,  0
+    ]
   end
 
   def col_moves
-    # rook col_moves + bishop col_moves
-    Array.new(14) {0} + (1..7).to_a + (-7..-1).to_a +
-      (1..7).to_a + (-7..-1).to_a.reverse + (-7..-1).to_a.reverse + (1..7).to_a
+    [
+      1,  2,  3,  4,  5,  6,  7,
+     -1, -2, -3, -4, -5, -6, -7,
+     -1, -2, -3, -4, -5, -6, -7,
+      1,  2,  3,  4,  5,  6,  7,
+      0,  0,  0,  0,  0,  0,  0,
+      0,  0,  0,  0,  0,  0,  0,
+      1,  2,  3,  4,  5,  6,  7,
+     -7, -6, -5, -4, -3, -2, -1
+    ]
   end
 end
